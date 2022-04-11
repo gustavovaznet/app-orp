433a7bda36b075f8fd66cda84566bd1189ca1ddd
gustavovaznet <98855318+gustavovaznet@users.noreply.github.com>
2022-04-11 11:17:56 -0300

    Create application files

---
+++ src/routes.ts
@@ -0,0 +1,17 @@
+//ROUTES
+
+//IMPORTING
+import { Router } from 'express';
+import multer from 'multer';
+import multerConfig from './config/multer'; 
+import OrphanagesController from './controllers/OrphanagesController';
+
+const routes = Router();
+const upload = multer(multerConfig);
+
+//ROUTES
+routes.get('/orphanages', OrphanagesController.index);
+routes.get('/orphanages/:id', OrphanagesController.show);
+routes.post('/orphanages', upload.array('images'), OrphanagesController.create);
+
+export default routes;
