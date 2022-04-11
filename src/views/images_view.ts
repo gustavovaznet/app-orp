280463ba835375b82a76955f837516a98886e168
gustavovaznet <98855318+gustavovaznet@users.noreply.github.com>
2022-04-11 11:22:02 -0300

    Upload MVC files

---
+++ src/views/images_view.ts
@@ -0,0 +1,14 @@
+//IMAGES VIEW
+
+//IMPORTING
+import Image from '../models/Image';
+
+export default {
+  render(image: Image) {
+    return `http://localhost:3333/images/${image.path}`;
+  },
+
+  renderMany(images: Image[]) {
+    return images.map(image => this.render(image));
+  }
+}
